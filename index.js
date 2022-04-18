ca41f016b4c9654c69274d3a019726c09cff9431
LilJuiceBox491 <74076575+LilJuiceBox491@users.noreply.github.com>
2022-04-18 17:23:11 -0400

    Finish Bot Initialization; Token is missing as of right now, and a bot name + prefix must be decided soon.

--- index.js
+++ index.js
@@ -20,8 +20,20 @@ const client = new Discord.Client({
 // Create Server App
 app.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<html><body><p>Jeffrey</p></body></html>");
+  res.write("<html><body><p>Sheesh</p></body></html>");
   res.end();
 });
 
 app.listen(5000);
+
+// Initialize the Bot Client
+client.once("ready", () => {
+  console.log("Bot is Ready");
+  console.log("Listening on Port");
+  client.user.setActivity("Jeffrey", {
+    type: "WATCHING"
+  });
+});
+
+// Login
+client.login("TOKEN GOES HERE")
